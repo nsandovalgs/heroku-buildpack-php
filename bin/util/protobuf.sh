f8c79dc8e1b048cb86612af53f2de2ad1318cadc
Tom Claus <mail@tomclaus.be>
2015-12-30 10:50:50 +0100

    Added curve25519

---
+++ bin/util/protobuf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Build Path: /app/.heroku/php/
+
+OUT_PREFIX=$1
+
+# fail hard
+set -o pipefail
+# fail harder
+set -eux
+
+echo "-----> Building protobuf..."
+
+curl -L https://codeload.github.com/allegro/php-protobuf/tar.gz/master | tar xz
+pushd php-protobuf-ext-master/
+phpize
+./configure --prefix=${OUT_PREFIX}
+make -s -j 9
+make install -s
+popd
+
+echo "-----> Done."
+
+install_ext "protobuf" "automatic"
+exts+=("protobuf")
