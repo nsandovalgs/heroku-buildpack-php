773affb9dc1da59650f0d3ea448ef8837f5b85fb
Nicolas Sandoval <nsandoval@grayshirts.com.ar>
2016-07-28 15:09:49 -0300

    Fix Crypto

--- bin/util/crypto.sh
+++ bin/util/crypto.sh
@@ -13,6 +13,8 @@ install_crypto_ext() {
     echo "-----> Building crypto..."
 
     curl --silent -L https://codeload.github.com/bukka/php-crypto/tar.gz/master | tar xz
+    curl --silent -L https://codeload.github.com/bukka/phpc/tar.gz/a9df7aeb092cced3df68794d453c6c1b360b5f68 | tar xz
+    cp phpc-a9df7aeb092cced3df68794d453c6c1b360b5f68/* php-crypto-master/phpc/
     pushd php-crypto-master/
     phpize >/dev/null
     ./configure --with-crypto >/dev/null
